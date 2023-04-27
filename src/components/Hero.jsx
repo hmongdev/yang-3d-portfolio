8e38a0fa93144c10c646f9b3f4a78d076e85d38d
Avery Yang <averyyang2674@gmail.com>
2023-04-26 20:17:36 -0500

    add intro

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,8 +9,37 @@ const Hero = () => {
 			className="relative w-full h-screen mx-auto border-4 border-red-600"
 		>
 			<div
-				className={`${styles.paddingX} absolute inset-0 top-[120px]`}
-			></div>
+				className={`${styles.paddingX} absolute inset-0 top-[120px] border-2 border-green-500 max-w-2xl mx-auto flex flex-row items-start gap-5`}
+			>
+				<div
+					name="circleContainer"
+					className="flex flex-col justify-center items-center mt-5"
+				>
+					<div
+						name="circle"
+						className="w-5 h-5 rounded-full bg-[#915eff]"
+					/>
+					<div className="w-1 sm:h-80 h-40 violet-gradient" />
+				</div>
+				<div name="introContainer">
+					<h1
+						name="introText"
+						className={`${styles.heroHeadText} text-white`}
+					>
+						Hi, I'm&nbsp;
+						<span className="text-[#915eff]">
+							Steve
+						</span>
+					</h1>
+					<div
+						className={`${styles.heroSubText}`}
+					>
+						I code websites, applications,
+						and build video games. Check out
+						my projects below!
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };
